eec7d98e45ac5fddc9e359e0949ebed50c1d3901
Avinash01 <avinash.m1302@gmail.com>
2024-02-27 12:28:36 +0530

    done

---
+++ script.js
@@ -0,0 +1,15 @@
+let div = document.getElementById("div");
+
+div.style.backgroundColor = "yellow";
+div.style.color = "Red";
+div.style.margin = "20px";
+div.style.padding = "10px";
+div.style.fontSize = "18px";
+div.style.fontWeight = "bold";
+div.style.height = "200px";
+div.style.width = "300px";
+div.style.display = "flex";
+div.style.justifyContent = "center";
+div.style.alignItems = "center";
+div.style.fontFamily = "Ubuntu, sans-serif";
+div.style.borderRadius = "25px";
